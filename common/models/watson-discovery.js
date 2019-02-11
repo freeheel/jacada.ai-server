bf31c5163db2598d0cb60cf5071173e94296eaac
freeheel <freeheel@gmail.com>
2019-02-11 09:13:14 +0100

    added watson discovery service

--- common/models/watson-discovery.js
+++ common/models/watson-discovery.js
@@ -3,8 +3,16 @@
 
 var DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 
-module.exports = function (WatsonDiscovery) {
+const PubNub = require('pubnub');
+const log = require('../util/logging').log('watson-discovery');
 
+let myPubnub = new PubNub({
+  publishKey: 'pub-c-9ef8635f-8eae-4a07-adb8-515edd926442',
+  subscribeKey: 'sub-c-2ebd149c-7e1c-11e5-9e96-02ee2ddab7fe',
+  ssl: true,
+});
+
+module.exports = function (WatsonDiscovery) {
 
   WatsonDiscovery.query = function (environmentId, collectionId, url, username, password, queryText, cb) {
 
@@ -23,7 +31,6 @@ module.exports = function (WatsonDiscovery) {
 
   };
 
-
   WatsonDiscovery.remoteMethod('query', {
     accepts: [
       {arg: 'environmentId', type: 'string'},
@@ -38,6 +45,40 @@ module.exports = function (WatsonDiscovery) {
     },
     http: {
       verb: 'get',
+    },
+  });
+
+  WatsonDiscovery.triggerSearch = function (spui, query, cb) {
+
+    log.info('triggerSearch for spui %s and query %s', spui, query);
+
+    myPubnub.publish({
+      channel: 'TriggerSearch - ' + spui,
+      message: {
+        name: 'KnowledgeSearch',
+        query: query,
+      },
+    }, function (status, response) {
+      if (status.error) {
+        log.error('Error sending triggerSearch', status);
+        cb(status.error);
+      } else {
+        log.debug('triggerSearch send');
+        cb(null, {
+          sendAt: response.timetoken,
+        });
+      }
+    });
+
+  };
+
+  WatsonDiscovery.remoteMethod('triggerSearch', {
+    accepts: [
+      {arg: 'spui', type: 'string'},
+      {arg: 'query', type: 'string'},
+    ],
+    http: {
+      verb: 'post',
     },
   });
 
